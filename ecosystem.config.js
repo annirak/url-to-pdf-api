05c27e1598d1f9c87bbc84b4f0a7545a04c6993f
Karina <karina.ruiz@outlook.com>
2019-04-18 14:24:28 -0400

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apps: [{
+    name: 'url-to-pdf',
+    script: 'src/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    exp_backoff_restart_delay: 100,
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development',
+      PORT: 9000,
+      ALLOW_HTTP: true,
+      DEBUG_MODE: false
+    },
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 9000,
+      ALLOW_HTTP: true,
+      DEBUG_MODE: false
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'node',
+      host: '212.83.163.1',
+      ref: 'origin/master',
+      repo: 'git@github.com:repo.git',
+      path: '/var/www/production',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
