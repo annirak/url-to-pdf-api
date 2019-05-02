0102bf6e03fb90c742260f13eda3520ca11a802a
Karina <karina.ruiz@outlook.com>
2019-05-02 10:41:34 -0400

    Format log stampdate, specific log level and increase body limit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps: [{
     name: 'url-to-pdf',
     script: 'src/index.js',
-
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     exp_backoff_restart_delay: 100,
     instances: 1,
@@ -12,17 +11,20 @@ module.exports = {
     output: './logs/out.log',
     error: './logs/error.log',
     log: './logs/combined.outerr.log',
+    log_date_format: 'YYYY-MM-DD HH:mm:ss',
     env: {
       NODE_ENV: 'development',
       PORT: 9000,
       ALLOW_HTTP: true,
-      DEBUG_MODE: false
+      DEBUG_MODE: false,
+      LOG_LEVEL: 'debug'
     },
     env_production: {
       NODE_ENV: 'production',
       PORT: 9000,
       ALLOW_HTTP: true,
-      DEBUG_MODE: false
+      DEBUG_MODE: false,
+      LOG_LEVEL: 'debug'
     }
   }],
 
