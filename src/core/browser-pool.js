178890c550b6b2d08dc247df5c64223a56b97f20
Karina <karina.ruiz@outlook.com>
2019-05-16 12:29:43 -0400

    Change pool to pages

--- src/core/browser-pool.js
+++ src/core/browser-pool.js
@@ -5,28 +5,28 @@ const config = require('../config');
 
 const poolFactory = {
   create() {
-    logger.info('Adding new browser to pool');
+    logger.info('Adding new page to pool');
     return puppeteer.launch({
       headless: !config.DEBUG_MODE,
       ignoreHTTPSErrors: false, // opts.ignoreHttpsErrors,
       args: ['--disable-gpu', '--no-sandbox', '--disable-setuid-sandbox'],
       sloMo: config.DEBUG_MODE ? 250 : undefined,
-    });
+    }).then(browser => browser.pages().then(pages => (pages ? pages[0] : browser.newPage())));
   },
   destroy(client) {
-    logger.info('Destroying browser from pool');
-    return client.close();
+    logger.info('Destroying page from pool');
+    return client.browser().close();
   },
 };
 
 const poolOpts = {
-  max: 10,
-  min: 4,
-  // evictionRunIntervalMillis: 60 * 60000,
+  max: 40,
+  min: 5,
+  evictionRunIntervalMillis: 60 * 60000,
   // numTestsPerEvictionRun: 5,
   // softIdleTimeoutMillis: 1 * 60000,
 };
-
+logger.info(`creating pool ${JSON.stringify(poolOpts)}`);
 const myPool = genericPool.createPool(poolFactory, poolOpts);
 
 module.exports = myPool;
